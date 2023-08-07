df81fb1427ca40bc34fafe7bde777b050d9bcace
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-07 11:34:52 -0500

    Pass down addOrder function as props to form; make ingredient buttons add onto P element when clicked

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getOrders } from "../../apiCalls";
+import getOrders from "../../apiCalls";
 import Orders from "../../components/Orders/Orders";
 import OrderForm from "../../components/OrderForm/OrderForm";
 
+
+// Iteration 3
+// Add functionality to enable form submission only when at least one ingredient and a name have been added to the order. If either of these two requirements is missing, the form should not be submittable. Upon successful submission, a POST request should be made to the server.
+
+// New orders should only be displayed on the page IF the POST request is successful. The user should see the new order displayed without the page refreshing. The new order should persist on the DOM after refreshing as well.
+
 function App() {
   const [orderData, setOrderData] = useState([])
+  const [newOrder, setNewOrder] = useState('')
 
   useEffect(() => {
     getOrders()
@@ -13,13 +20,16 @@ function App() {
     .catch(err => alert(err))
   }, []);
 
-  console.log('out',orderData)
+  const addOrder = (order) => {
+    setNewOrder(order)
+  }
+
 
   return (
     <main className="App">
       <header>
         <h1>Burrito Builder</h1>
-        <OrderForm  />
+        <OrderForm addOrder={addOrder} />
       </header>
 
       <Orders orderData={orderData} />
--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
 
-function OrderForm(props) {
+
+// Iteration 3
+// Add functionality to enable form submission only when at least one ingredient and a name have been added to the order. If either of these two requirements is missing, the form should not be submittable. Upon successful submission, a POST request should be made to the server.
+
+// New orders should only be displayed on the page IF the POST request is successful. The user should see the new order displayed without the page refreshing. The new order should persist on the DOM after refreshing as well.
+
+function OrderForm({ addOrder }) {
   const [name, setName] = useState("");
   const [ingredients, setIngredients] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
+    const newOrder = {
+      name: name, 
+      ingredients: ingredients,
+      id: Date.now()
+    }
+    addOrder(newOrder)
     clearInputs();
   }
 
@@ -28,17 +40,24 @@ function OrderForm(props) {
     "cilantro",
     "sour cream",
   ];
+
   const ingredientButtons = possibleIngredients.map((ingredient) => {
     return (
       <button
         key={ingredient}
+        value={ingredient}
         name={ingredient}
-        // onClick={(e) => }
+        onClick={(e) => {
+          e.preventDefault()
+          setIngredients([...ingredients, e.target.value])
+        }}
       >
         {ingredient}
       </button>
     );
   });
+
+  console.log('ingredients',ingredients)
 
   return (
     <form>
@@ -47,7 +66,7 @@ function OrderForm(props) {
         placeholder="Name"
         name="name"
         value={name}
-        // onChange={(e) => }
+        onChange={(e) => setName(e.target.value)}
       />
 
       {ingredientButtons}
