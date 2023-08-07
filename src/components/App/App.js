1ea4da2ecce34d94edaaad86dda2ecf9fa8c169b
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-07 13:07:23 -0500

    Add loading state and conditional that renderselement if loading

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,20 +4,16 @@ import { getOrders, postOrder } from "../../apiCalls";
 import Orders from "../../components/Orders/Orders";
 import OrderForm from "../../components/OrderForm/OrderForm";
 
-
-// Iteration 3
-// Add functionality to enable form submission only when at least one ingredient and a name have been added to the order. If either of these two requirements is missing, the form should not be submittable. Upon successful submission, a POST request should be made to the server.
-
-// New orders should only be displayed on the page IF the POST request is successful. The user should see the new order displayed without the page refreshing. The new order should persist on the DOM after refreshing as well.
-
 function App() {
   const [orderData, setOrderData] = useState([])
   const [newOrder, setNewOrder] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getOrders()
     .then(data => setOrderData(data.orders))
     .catch(err => alert(err))
+    setLoading(false)
   }, []);
 
   const addOrder = (order) => {
@@ -43,7 +39,7 @@ function App() {
         <h1>Burrito Builder</h1>
         <OrderForm addOrder={addOrder} />
       </header>
-
+      {loading && <h2>Loading</h2>}
       <Orders orderData={orderData} />
     </main>
   );
