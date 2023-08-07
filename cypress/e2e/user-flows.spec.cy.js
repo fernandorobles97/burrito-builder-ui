461bdaea29684e5a22cbf5d16b52e271f91154ec
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-07 13:04:28 -0500

    Add fixtures and begin testing for user flow of adding order to DOM

--- cypress/e2e/user-flows.spec.cy.js
+++ cypress/e2e/user-flows.spec.cy.js
@@ -4,6 +4,12 @@ describe('user flows', () => {
       statusCode: 200,
       fixture: 'orders.json'
     }).as('getOrders')
+
+    cy.intercept("POST", "http://localhost:3001/api/v1/orders", {
+      statusCode: 200,
+      fixture: 'newOrder.json'
+    }).as('postOrder')
+    
     cy.visit('http://localhost:3000')
   })
 
@@ -12,6 +18,13 @@ describe('user flows', () => {
       cy.get('h1').contains('Burrito Builder')
       .get('form').children().should('have.length', 15)
       .get('.order-wrapper').children().should('have.length', 3)
+    })
+  })
+
+  // Write a test to check the user flow of adding a new order to the DOM.
+  it('should be able to add a new order', () => {
+    cy.wait('@getOrders').then((interception) => {
+      cy.get('input[type="text"]').type('Cat')
     })
   })
 
