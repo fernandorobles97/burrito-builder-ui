2a0b463a0742f4a3150cbe7a0674d283f4aeb8c7
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-08 11:59:31 -0500

    Remove commented out code

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -5,18 +5,6 @@ import OrderCard from "../OrderCard/OrderCard";
 const Orders = ({ orderData }) => {
 
   const orderEls = orderData.map(order => <OrderCard key={order.id} order={order} />)
-  // const orderEls = orderData.orders.map((order) => {
-  //   return (
-  //     <div className="order">
-  //       <h3>{order.name}</h3>
-  //       <ul className="ingredient-list">
-  //         {order.ingredients.map((ingredient) => {
-  //           return <li>{ingredient}</li>;
-  //         })}
-  //       </ul>
-  //     </div>
-  //   );
-  // });
 
   return (
     <section className="order-wrapper">
